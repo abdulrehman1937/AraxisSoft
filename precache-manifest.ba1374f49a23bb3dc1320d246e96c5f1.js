2981816b10d43eb0e29672123e3c1d1f7f2a7de9
Abdul Rehman <abdulrehman1937@hotmail.com>
2020-08-11 20:27:37 +0500

    Updates

---
+++ precache-manifest.ba1374f49a23bb3dc1320d246e96c5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429ce8faf607ab3f4e4cd7b3b9b8bb74",
+    "url": "/AraxisSoft/index.html"
+  },
+  {
+    "revision": "dc0a15d717945e625422",
+    "url": "/AraxisSoft/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a5b104c73ae42d6cb28d",
+    "url": "/AraxisSoft/static/js/2.29eb80a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AraxisSoft/static/js/2.29eb80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0a15d717945e625422",
+    "url": "/AraxisSoft/static/js/main.59f173d1.chunk.js"
+  },
+  {
+    "revision": "35c7872297625cbe932d",
+    "url": "/AraxisSoft/static/js/runtime-main.81e42d3f.js"
+  }
+]);
